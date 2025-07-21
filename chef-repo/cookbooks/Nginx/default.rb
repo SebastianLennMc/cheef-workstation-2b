76d0769fb7da9c839f79cd976ea40b14d0908d5a
Sebastian Rodriguez <31926153+SebastianLennMc@users.noreply.github.com>
2025-07-21 12:10:46 -0600

    default.rb

--- chef-repo/cookbooks/Nginx/default.rb
+++ chef-repo/cookbooks/Nginx/default.rb
@@ -6,6 +6,13 @@ service 'nginx' do
   action [:enable, :start]
 end
 
+directory '/var/www/misweb/html' do
+  recursive true
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
 file '/var/www/misweb/html/index.html' do
   content '<h1>Servidor Nginx configurado por Sebastian con Chef</h1>'
   mode '0644'
